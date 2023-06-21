82669e16a763fc29ccfaa3142c79fca4ba0bfa84
MinwooHconnect <kmwdev@hconnect.co.kr>
2023-06-21 11:26:19 +0900

    - 뺄셈, 곱셈 추가 / group test 추가

--- test/maths_util_test.dart
+++ test/maths_util_test.dart
@@ -2,15 +2,42 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tdd_practice/maths_util.dart';
 
 void main() {
-  test('check for two number addition', () {
-    // Arrage
-    var a = 10;
-    var b = 10;
 
-    // Act
-    var sum = add(a, b);
+  group('Maths utils', () {
+    test('check for two number addition', () {
+      // Arrage
+      var a = 10;
+      var b = 10;
 
-    // Assert
-    expect(sum, 20);
+      // Act
+      var sum = add(a, b);
+
+      // Assert
+      expect(sum, 20);
+    });
+
+    test('check for two number subtract', () {
+      // Arrage
+      var a = 10;
+      var b = 10;
+
+      // Act
+      var sub = substract(a, b);
+
+      // Assert
+      expect(sub, 0);
+    });
+
+    test('check for two number muntiply', () {
+      // Arrage
+      var a = 10;
+      var b = 10;
+
+      // Act
+      var mul = multiply(a, b);
+
+      // Assert
+      expect(mul, 100);
+    });
   });
 }
